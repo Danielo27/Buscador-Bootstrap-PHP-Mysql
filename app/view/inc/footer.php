6a98b407fc1e5674ca3dfd6c26d7711feb60e83b
Daniel Quintero Henriquez <66169527+Danielo27@users.noreply.github.com>
2023-06-23 15:41:20 -0500

    delete old files

--- app/view/inc/footer.php
+++
@@ -1,5 +0,0 @@
-	<script type="text/javascript" src="<?php echo JS ?>bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php echo JS ?>bootstrap.bundle.min.js"></script>
-	<script type="text/javascript" src="<?php echo JS ?>jquery.min.js"></script>
-	</body>
-	</html>
